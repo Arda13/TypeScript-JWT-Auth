34b5962ea7786af3dcf2ddf1adeef0b7e027f5a0
Arda Kaya <ardaka23@outlook.com>
2020-11-15 22:19:40 +0300

    End of 1st vid

---
+++ packages/frontend/src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+type Props = {
+    children: React.ReactNode;
+}
+export default function Layout({children}: Props) {
+    return <div className="container mx-auto">
+        <Header></Header>
+        {children}
+    </div>
+}
---
+++ packages/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Layout from "./components/Layout";
+import Login from "./components/Login";
+
+function App() {
+
+}
+ReactDOM.render(<Layout><Login></Login></Layout>, document.getElementById("root")!);
